2210cf406a67b593301d2889b5dff32cfbf4a4fb
Nicholas Nassar <niccholaspage@gmail.com>
2012-07-28 12:30:00 -0400

    Change it to removed

--- src/org/melonbrew/fe/Phrase.java
+++ src/org/melonbrew/fe/Phrase.java
@@ -22,7 +22,7 @@ public enum Phrase {
 	ACCOUNT_CREATED_GRANT("Created an account for $1 and granted it $2"),
 	NO_ACCOUNTS_EXIST("No accounts exist."),
 	NAME_TOO_LONG("Sorry that name is too long."),
-	ACCOUNT_CLEANED("All accounts with the default balance have been cleared.");
+	ACCOUNT_CLEANED("All accounts with the default balance have been removed.");
 
 	private String defaultMessage;
 
