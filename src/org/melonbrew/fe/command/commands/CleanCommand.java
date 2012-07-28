21147ff25ec069e8fa259132e0a6ff6875f9a51b
Nicholas Nassar <niccholaspage@gmail.com>
2012-07-27 23:05:18 -0400

    Actually add in clean functionality. Oops

--- src/org/melonbrew/fe/command/commands/CleanCommand.java
+++ src/org/melonbrew/fe/command/commands/CleanCommand.java
@@ -3,6 +3,7 @@ package org.melonbrew.fe.command.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.melonbrew.fe.Fe;
+import org.melonbrew.fe.Phrase;
 import org.melonbrew.fe.command.CommandType;
 import org.melonbrew.fe.command.SubCommand;
 
@@ -17,6 +18,8 @@ public class CleanCommand extends SubCommand {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String[] args){
 		plugin.getAPI().clean();
+		
+		sender.sendMessage(plugin.getMessagePrefix() + Phrase.ACCOUNT_CLEANED.parse());
 		
 		return true;
 	}
