1e3ac039e48e369c0267b1147393c4be24d9e217
Nicholas Nassar <niccholaspage@gmail.com>
2012-08-03 21:56:00 -0400

    Add in SQLite DB and make database loading done through a HashMap

---
+++ src/org/melonbrew/fe/database/databases/SQLiteDB.java
@@ -0,0 +1,20 @@
+package org.melonbrew.fe.database.databases;
+
+import lib.PatPeter.SQLibrary.Database;
+import lib.PatPeter.SQLibrary.SQLite;
+
+import org.melonbrew.fe.Fe;
+
+public class SQLiteDB extends SQLDB {
+	private final Fe plugin;
+	
+	public SQLiteDB(Fe plugin){
+		super(plugin);
+		
+		this.plugin = plugin;
+	}
+	
+	public Database getNewDatabase(){
+		return new SQLite(plugin.getLogger(), "Fe", "database", plugin.getDataFolder().getPath());
+	}
+}
