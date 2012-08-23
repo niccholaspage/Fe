7dae81b1103b440621a6a498a4d903e59fe984e2
Nicholas Nassar <niccholaspage@gmail.com>
2012-08-23 14:02:28 -0400

    Actually register command

--- src/org/melonbrew/fe/FeCommand.java
+++ src/org/melonbrew/fe/FeCommand.java
@@ -31,6 +31,7 @@ public class FeCommand implements CommandExecutor {
 		commands.add(new SetCommand(plugin));
 		commands.add(new GrantCommand(plugin)); 
 		commands.add(new CleanCommand(plugin));
+		commands.add(new ConvertCommand(plugin));
 		commands.add(new ReloadCommand(plugin));
 	}
 	
