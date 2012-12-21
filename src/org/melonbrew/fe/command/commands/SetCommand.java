940043513aef144608f943afec0c73acf951f5ab
OdiumxXx <cam.ofc@gmail.com>
2012-12-22 03:14:10 +1000

    Updated Set to getShortenedAccount as is done with Send/Balance
    Seeing as /fe deduct was rejected I'll apply the getShotenedAccount changes one at a time.

--- src/org/melonbrew/fe/command/commands/SetCommand.java
+++ src/org/melonbrew/fe/command/commands/SetCommand.java
@@ -10,38 +10,44 @@ import org.melonbrew.fe.database.Account;
 
 public class SetCommand extends SubCommand {
 	private final Fe plugin;
-	
+
 	public SetCommand(Fe plugin){
 		super("set", "fe.set", "set [name] [amount]", Phrase.COMMAND_SET, CommandType.CONSOLE);
-		
+
 		this.plugin = plugin;
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		if (args.length < 2){
 			return false;
 		}
-		
+
 		double money;
-		
+
 		try {
 			money = Double.parseDouble(args[1]);
 		} catch (NumberFormatException e){
 			return false;
 		}
-		
-		Account victim = plugin.getAPI().getAccount(args[0]);
-		
+
+		Account victim = plugin.getShortenedAccount(args[0]);
+
 		if (victim == null){
-			Phrase.ACCOUNT_DOES_NOT_EXIST.sendWithPrefix(sender);
-			
+			Phrase.ACCOUNT_DOES_NOT_EXIST.sendWithPrefix(sender);			
 			return true;
 		}
-		
-		victim.setMoney(money);
-		
-		Phrase.PLAYER_SET_MONEY.sendWithPrefix(sender, plugin.getReadName(victim), plugin.getAPI().format(victim));
-		
+
+		String victimName = plugin.getReadName(victim);
+
+		if (!victim.canRecieve(money)){
+			Phrase.MAX_BALANCE_REACHED.sendWithPrefix(sender, victimName);
+			return true;
+		}
+
+		String formattedMoney = plugin.getAPI().format(money);
+		victim.setMoney(money);		
+		Phrase.PLAYER_SET_MONEY.sendWithPrefix(sender, plugin.getReadName(victim), formattedMoney);
+
 		return true;
 	}
 }
