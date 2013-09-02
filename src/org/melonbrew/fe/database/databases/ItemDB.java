6bf5454119a994ba6140474012ca6057fc2147a4
Nicholas Nassar <niccholaspage@gmail.com>
2013-09-02 13:31:23 -0400

    A start to an item database

---
+++ src/org/melonbrew/fe/database/databases/ItemDB.java
@@ -0,0 +1,84 @@
+package org.melonbrew.fe.database.databases;
+
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.melonbrew.fe.Fe;
+import org.melonbrew.fe.database.Account;
+import org.melonbrew.fe.database.Database;
+
+public class ItemDB extends Database {
+	private final Fe plugin;
+
+	public ItemDB(Fe plugin) {
+		super(plugin);
+
+		this.plugin = plugin;
+	}
+
+	public boolean init() {
+		return false;
+	}
+
+	@Override
+	public List<Account> getTopAccounts(int size){
+		return null;
+	}
+
+	@Override
+	public List<Account> getAccounts(){
+		return null;
+	}
+
+	@Override
+	public double loadAccountMoney(String name){
+		Player player = plugin.getServer().getPlayerExact(name);
+
+		if (player == null){
+			return 0;
+		}
+
+		ItemStack[] items = player.getInventory().getContents();
+
+		double money = 0;
+
+		for (ItemStack stack : items){
+			if (stack != null && stack.getType() == Material.GOLD_INGOT){
+				money += stack.getAmount();
+			}
+		}
+
+		return money * 10;
+	}
+
+	@Override
+	protected void saveAccount(String name, double money){
+
+	}
+
+	@Override
+	public void removeAccount(String name){
+
+	}
+
+	@Override
+	public void getConfigDefaults(ConfigurationSection section){
+
+	}
+
+	@Override
+	public void clean(){
+	}
+
+	@Override
+	public void close(){
+	}
+
+	@Override
+	public String getName(){
+		return "Item";
+	}
+}
