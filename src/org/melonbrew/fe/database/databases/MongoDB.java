f60bf468d631f19479193bd024156673e0982241
Nicholas Nassar <niccholaspage@gmail.com>
2013-09-02 01:27:31 -0400

    MongoDB support!

---
+++ src/org/melonbrew/fe/database/databases/MongoDB.java
@@ -0,0 +1,160 @@
+package org.melonbrew.fe.database.databases;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.melonbrew.fe.Fe;
+import org.melonbrew.fe.database.Account;
+import org.melonbrew.fe.database.Database;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+public class MongoDB extends Database {
+	private final Fe plugin;
+
+	private MongoClient mongoClient;
+
+	private static final String ACCOUNTS_COLLECTION = "accounts";
+
+	public MongoDB(Fe plugin) {
+		super(plugin);
+
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean init() {
+		try {
+			mongoClient = new MongoClient(getConfigSection().getString("host"), getConfigSection().getInt("port"));
+		} catch (UnknownHostException e){
+			return false;
+		}
+
+		if (getDatabase() == null || !getDatabase().isAuthenticated()){
+			return false;
+		}
+
+		return true;
+	}
+
+	public DB getDatabase(){
+		DB database = mongoClient.getDB(getConfigSection().getString("database"));
+
+		database.authenticate(getConfigSection().getString("user"), getConfigSection().getString("password").toCharArray());
+
+		return database;
+	}
+
+	public double loadAccountMoney(String name){
+		DBCollection collection = getDatabase().getCollection(ACCOUNTS_COLLECTION);
+
+		DBObject userObject = collection.findOne(new BasicDBObject("name", name));
+
+		if (userObject == null){
+			return -1;
+		}
+
+		return ((BasicDBObject) userObject).getDouble("money");
+	}
+
+	public void removeAccount(String name){
+		DBCollection collection = getDatabase().getCollection(ACCOUNTS_COLLECTION);
+
+		DBObject oldUserObject = collection.findOne(new BasicDBObject("name", name));
+
+		if (oldUserObject != null){
+			collection.remove(oldUserObject);
+		}
+	}
+
+	public void saveAccount(String name, double money){
+		DBCollection collection = getDatabase().getCollection(ACCOUNTS_COLLECTION);
+
+		DBObject oldUserObject = collection.findOne(new BasicDBObject("name", name));
+
+		if (oldUserObject != null){
+			collection.remove(oldUserObject);
+		}
+
+		collection.insert(new BasicDBObject("name", name).append("money", money));
+	}
+
+	@Override
+	public void getConfigDefaults(ConfigurationSection section) {
+		section.addDefault("host", "localhost");
+
+		section.addDefault("port", 27017);
+
+		section.addDefault("user", "root");
+
+		section.addDefault("password", "minecraft");
+
+		section.addDefault("database", "Fe");
+	}
+
+	@Override
+	public String getName() {
+		return "Mongo";
+	}
+
+	@Override
+	public List<Account> getTopAccounts(int size) {
+		DBCursor cursor = getDatabase().getCollection(ACCOUNTS_COLLECTION).find().sort(new BasicDBObject("money", -1)).limit(size);
+
+		List<Account> topAccounts = new ArrayList<Account>();
+
+		Iterator<DBObject> iterator = cursor.iterator();
+
+		while (iterator.hasNext()){
+			BasicDBObject topAccountObject = (BasicDBObject) iterator.next();
+
+			Account account = new Account(topAccountObject.getString("name"), plugin, this);
+
+			account.setMoney(topAccountObject.getDouble("money"));
+
+			topAccounts.add(account);
+		}
+
+		return topAccounts;
+	}
+
+	@Override
+	public List<Account> getAccounts() {
+		DBCursor cursor = getDatabase().getCollection(ACCOUNTS_COLLECTION).find().sort(new BasicDBObject("money", -1));
+
+		List<Account> accounts = new ArrayList<Account>();
+
+		Iterator<DBObject> iterator = cursor.iterator();
+
+		while (iterator.hasNext()){
+			BasicDBObject accountObject = (BasicDBObject) iterator.next();
+
+			Account account = new Account(accountObject.getString("name"), plugin, this);
+
+			account.setMoney(accountObject.getDouble("money"));
+
+			accounts.add(account);
+		}
+
+		return accounts;
+	}
+
+	public void clean() {
+		DBCursor cursor = getDatabase().getCollection(ACCOUNTS_COLLECTION).find().sort(new BasicDBObject("money", plugin.getAPI().getDefaultHoldings()));
+
+		cursor.remove();
+	}
+
+	@Override
+	public void close() {
+
+	}
+}
--- src/org/melonbrew/fe/database/databases/SQLDB.java
+++ src/org/melonbrew/fe/database/databases/SQLDB.java
@@ -16,57 +16,57 @@ import com.niccholaspage.nSQL.query.SelectQuery;
 
 public abstract class SQLDB extends Database {
 	private final Fe plugin;
-	
+
 	private final boolean supportsModification;
-	
+
 	private Connection connection;
-	
+
 	private String accountsName;
-	
+
 	private Table accounts;
-	
+
 	public SQLDB(Fe plugin, boolean supportsModification){
 		super(plugin);
-		
+
 		this.plugin = plugin;
-		
+
 		this.supportsModification = supportsModification;
-		
+
 		accountsName = "fe_accounts";
 	}
-	
+
 	public void setAccountTable(String accountsName){
 		this.accountsName = accountsName;
 	}
-	
+
 	public String getAccountsName(){
 		return accountsName;
 	}
-	
+
 	public boolean init(){
 		if (!checkConnection()){
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean checkConnection(){
 		try {
 			if (connection == null || connection.isClosed()){
 				connection = getNewConnection();
-				
+
 				if (connection == null || connection.isClosed()){
 					return false;
 				}
-				
+
 				accounts = new Table(connection, accountsName);
-				
+
 				accounts.create().create("name varchar(64) NOT NULL").create("money double NOT NULL").execute();
 
 				if (supportsModification){
 					query("ALTER TABLE " + accountsName + " MODIFY name varchar(64) NOT NULL");
-					
+
 					query("ALTER TABLE " + accountsName + " MODIFY money double NOT NULL");
 				}
 			}
@@ -75,26 +75,26 @@ public abstract class SQLDB extends Database {
 
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	protected abstract Connection getNewConnection();
-	
+
 	public boolean query(String sql){
 		try {
 			return connection.createStatement().execute(sql);
 		} catch (SQLException e){
 			e.printStackTrace();
-			
+
 			return false;
 		}
 	}
-	
+
 	public Connection getConnection(){
 		return connection;
 	}
-	
+
 	public void close(){
 		try {
 			if (connection != null)
@@ -103,126 +103,126 @@ public abstract class SQLDB extends Database {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public List<Account> getTopAccounts(int size){
 		checkConnection();
-		
+
 		String sql = "SELECT * FROM " + accountsName + " ORDER BY money DESC limit " + size;
-		
+
 		List<Account> topAccounts = new ArrayList<Account>();
-		
+
 		try {
 			ResultSet set = connection.createStatement().executeQuery(sql);
-			
+
 			while (set.next()){
 				Account account = new Account(set.getString("name").toLowerCase(), plugin, this);
-				
+
 				account.setMoney(set.getDouble("money"));
-				
-				topAccounts.add(account);	
+
+				topAccounts.add(account);
 			}
 		} catch (SQLException e){
 			e.printStackTrace();
 		}
-		
+
 		return topAccounts;
 	}
-	
+
 	public List<Account> getAccounts(){
 		checkConnection();
-		
+
 		List<Account> accounts = new ArrayList<Account>();
-		
+
 		ResultSet set = this.accounts.select().execute();
-		
+
 		try {
 			while (set.next()){
 				Account account = new Account(set.getString("name").toLowerCase(), plugin, this);
-				
+
 				account.setMoney(set.getDouble("money"));
-				
+
 				accounts.add(account);
 			}
 		} catch (SQLException e){
 			e.printStackTrace();
 		}
-		
+
 		return accounts;
 	}
-	
+
 	public double loadAccountMoney(String name){
 		checkConnection();
-		
+
 		double money = -1;
-		
+
 		try {
 			SelectQuery query = accounts.select().where("name", name);
-			
+
 			ResultSet set = query.execute();
-			
+
 			while (set.next()){
 				money = set.getDouble("money");
 			}
-			
+
 			query.close();
 		} catch (SQLException e){
 			e.printStackTrace();
-			
+
 			return -1;
 		}
-		
+
 		return money;
 	}
-	
+
 	public void removeAccount(String name){
 		checkConnection();
-		
+
 		accounts.delete().where("name", name).execute();
 	}
-	
+
 	protected void saveAccount(String name, double money){
 		checkConnection();
-		
+
 		if (accountExists(name)){
 			accounts.update().set("money", money).where("name", name).execute();
 		}else {
 			accounts.insert().insert("name").insert("money").value(name).value(money).execute();
 		}
 	}
-	
+
 	public void clean(){
 		checkConnection();
-		
+
 		try {
 			SelectQuery query = accounts.select().where("money", plugin.getAPI().getDefaultHoldings());
-			
+
 			ResultSet set = query.execute();
-			
+
 			boolean executeQuery = false;
-			
+
 			StringBuilder builder = new StringBuilder("DELETE FROM " + accountsName + " WHERE name IN (");
-			
+
 			while (set.next()){
 				String name = set.getString("name");
-				
+
 				if (plugin.getServer().getPlayerExact(name) != null){
 					continue;
 				}
-				
+
 				executeQuery = true;
-				
+
 				builder.append("'").append(name).append("', ");
 			}
-			
+
 			set.close();
-			
+
 			builder.delete(builder.length() - 2, builder.length()).append(")");
-			
+
 			if (executeQuery){
 				query(builder.toString());
 			}
 		} catch (SQLException e){
-			
+
 		}
 	}
 }
