ca4e2f6f73718b2a30b1b1e218f2350af4590719
Nicholas <niccholaspage@gmail.com>
2014-04-30 19:15:26 -0400

    Remove Item database since there are much better plugins to do this, like GoldIsMoney

--- src/org/melonbrew/fe/Fe.java
+++ src/org/melonbrew/fe/Fe.java
@@ -18,7 +18,6 @@ import org.melonbrew.fe.Metrics.Graph;
 import org.melonbrew.fe.Metrics.Plotter;
 import org.melonbrew.fe.database.Account;
 import org.melonbrew.fe.database.Database;
-import org.melonbrew.fe.database.databases.ItemDB;
 import org.melonbrew.fe.database.databases.MongoDB;
 import org.melonbrew.fe.database.databases.MySQLDB;
 import org.melonbrew.fe.database.databases.SQLiteDB;
@@ -49,7 +48,6 @@ public class Fe extends JavaPlugin {
 		databases.add(new MySQLDB(this));
 		databases.add(new SQLiteDB(this));
 		databases.add(new MongoDB(this));
-		databases.add(new ItemDB(this));
 
 		for (Database database : databases){
 			String name = database.getConfigName();
@@ -73,7 +71,7 @@ public class Fe extends JavaPlugin {
 				"holdings - The amount of money that players will start out with\n" +
 				"prefix - The message prefix\n" +
 				"currency - The single and multiple names for the currency\n" +
-				"type - The type of database used (sqlite, mysql, mongo, or item)\n" +
+				"type - The type of database used (sqlite, mysql, or mongo)\n" +
 				"updatecheck - Checks if there is an update to Fe" +
 				"cacheaccounts - Caches players' balances when logged on to the server");
 
