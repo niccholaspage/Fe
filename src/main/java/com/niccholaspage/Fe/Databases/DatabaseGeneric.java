9e9a39778e54bdc6881c417c084a22cc3bd17eed
SimSonic <simsonic@yandex.ru>
2015-07-13 16:20:21 +0600

    Completely remove UUIDFetcher. Pligin discover nicknames/UUIDs from PlayerLoginEvent.

--- src/main/java/com/niccholaspage/Fe/Databases/DatabaseGeneric.java
+++ src/main/java/com/niccholaspage/Fe/Databases/DatabaseGeneric.java
@@ -124,44 +124,4 @@ public abstract class DatabaseGeneric implements Database
 	public abstract void cleanAccountsWithDefaultHoldings();
 	@Override
 	public abstract void removeAllAccounts();
-	/*
-	protected boolean convertToUUID()
-	{
-		if(!plugin.getServer().getOnlineMode())
-		{
-			// Disable plugin?
-		}
-		plugin.log(Phrases.STARTING_UUID_CONVERSION);
-		List<Account> accounts = loadAccounts();
-		Map<String, Double> accountMonies = new HashMap<>();
-		for(AccountInt account : accounts)
-			accountMonies.put(account.getName(), account.getMoney());
-		List<String> names = new ArrayList<>();
-		for(AccountInt account : accounts)
-			names.add(account.getName());
-		try
-		{
-			UUIDFetcher fetcher = new UUIDFetcher(names);
-			Map<String, UUID> response = fetcher.call();
-			removeAllAccounts();
-			for(String name : response.keySet())
-				for(String accountName : new HashMap<>(accountMonies).keySet())
-					if(accountName.equalsIgnoreCase(name))
-					{
-						saveAccount(name, response.get(name).toString(), accountMonies.get(accountName));
-						accountMonies.remove(accountName);
-					}
-			for(String accountName : accountMonies.keySet())
-				saveAccount(accountName, null, accountMonies.get(accountName));
-		} catch(Exception e)
-		{
-			System.out.println(e);
-			plugin.log(Phrases.UUID_CONVERSION_FAILED);
-			plugin.getServer().getPluginManager().disablePlugin(plugin);
-			return false;
-		}
-		plugin.log(Phrases.UUID_CONVERSION_SUCCEEDED);
-		return true;
-	}
-	*/
 }
